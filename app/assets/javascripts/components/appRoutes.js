c7a5a0007ff1bc0c7f221e0f7ffcac9c79176b38
reyes rodriguez rodriguez <reyesorodriguez@gmail.com>
2016-04-18 10:14:14 -0400

    heroku

---
+++ app/assets/javascripts/components/appRoutes.js
@@ -0,0 +1,10 @@
+// var app = angular.module('single-page-app',['ngRoute']);
+// app.config(function($routeProvider){
+// 	$routeProvider
+// 		.when('/home',{
+// 			templateUrl: 'index.html.erb'
+// 		})
+// 		.when('/venta',{
+// 			templateUrl:  'venta.html.erb'
+// 		});
+// });
